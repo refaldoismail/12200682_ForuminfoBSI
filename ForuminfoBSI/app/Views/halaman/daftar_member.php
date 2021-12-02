d035316157ba815f167467e4aafa75c53e48b246
12200907siskasera <siskasera09@gmail.com>
2021-12-02 13:54:22 +0700

    Final Project Bagian 1

---
+++ ForuminfoBSI/app/Views/halaman/daftar_member.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Selamat Datang teman teman</title>
+</head>
+<body>
+    saya akan menampilkan daftar info bsi
+</body>
+</html>
---
+++ ForuminfoBSI/app/Views/halaman/login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ForumInfoBSI</title>
+</head>
+<body>
+    <p>
+    disini kita membuat info forums BSI
+    <a href="<?=base_url('/daftar-member')?>">klik daftar</a> untuk memulai forum
+    </p>
+</body>
+</html>
