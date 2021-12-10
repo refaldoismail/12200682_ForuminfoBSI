19e4378c042743af8240765c2c97f33201c88947
refaldoismail <refaldo182@gmail.com>
2021-12-10 15:01:12 +0700

    Final Project Bagian 5

--- ForuminfoBSI/app/Models/Pengguna.php
+++ ForuminfoBSI/app/Models/Pengguna.php
@@ -7,7 +7,7 @@ use CodeIgniter\Model;
 class Pengguna extends Model
 {
     protected $DBGroup          = 'koneksiku';
-    protected $table            = 'Pengguna';
+    protected $table            = 'pengguna';
     protected $primaryKey       = 'id';
     protected $useAutoIncrement = true;
     protected $returnType       = 'array';
